6e5f035bdadedca76e2e2e44059dbb410ab6d0c9
trinhhoa <trinhdinhhoa321@gmail.com>
2023-02-27 15:02:10 +0700

    updateok

--- apps/admin/src/constants/config.ts
+++ apps/admin/src/constants/config.ts
@@ -1,4 +1,3 @@
-// export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const BASE_URL = "http://localhost:6082";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const LOCAL_ROLE = "role";
 export const LOCAL_TOKEN = "token";
