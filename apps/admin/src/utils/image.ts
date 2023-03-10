b6fcc40a499d422c3172270c8f0bc0710089e33c
Canh Nguyen Tran <canhnt@aiacademy.edu.vn>
2023-03-10 11:52:45 +0700

    And if you ask me how I'm feeling

--- apps/admin/src/utils/image.ts
+++ apps/admin/src/utils/image.ts
@@ -1,5 +1,5 @@
 import { BASE_URL } from "@/constants/config";
 
-export function generateImage(url: string, prefix = "/") {
+export function generateImage(url: string, prefix = "/v2/") {
   return BASE_URL + prefix + url;
 }
