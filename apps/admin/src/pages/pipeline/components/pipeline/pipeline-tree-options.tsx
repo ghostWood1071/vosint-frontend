ae702a0879a818269c7fd91db45fc251394104b3
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2023-03-28 16:53:39 +0700

    Feat: add type pubdate

--- apps/admin/src/pages/pipeline/components/pipeline/pipeline-tree-options.tsx
+++ apps/admin/src/pages/pipeline/components/pipeline/pipeline-tree-options.tsx
@@ -1,6 +1,7 @@
 import { IActionParamInfo } from "@/services/pipeline.type";
-import { CloseCircleOutlined } from "@ant-design/icons";
-import { Card, Form, Input, Select } from "antd";
+import { CloseCircleOutlined, MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Card, Form, Input, Select, Space } from "antd";
+import classNames from "classnames";
 import { useEffect } from "react";
 
 import { usePipelineSource } from "../../pipeline.loader";
@@ -31,47 +32,77 @@ export function PipelineTreeOptions({ option, onClose, onValuesChange }: Props) 
   return (
     <Card title="Options" className={styles.root} extra={<CloseCircleOutlined onClick={onClose} />}>
       <Form form={form} layout="vertical" onValuesChange={onValuesChange}>
-        {option?.param_infos?.map((param) => (
-          <RenderFormItem key={param.name} param={param} />
-        ))}
+        {option?.param_infos?.map((param) => renderFormItemStrategies?.[param?.val_type]?.(param))}
       </Form>
     </Card>
   );
 }
 
-function RenderFormItem({ param }: { param: IActionParamInfo }) {
+const renderFormItemStrategies: Record<string, (props: IActionParamInfo) => JSX.Element> = {
+  select: ({ options, name, display_name }) => (
+    <Form.Item key={name} label={display_name} name={[name]}>
+      <Select>
+        {options?.map((val) => (
+          <Option key={val} value={val}>
+            {val}
+          </Option>
+        ))}
+      </Select>
+    </Form.Item>
+  ),
+  str: ({ name, display_name }) => (
+    <Form.Item key={name} label={display_name} name={[name]}>
+      <TextArea />
+    </Form.Item>
+  ),
+  source: SourceItem,
+  pubdate: PubDateItem,
+};
+
+function PubDateItem({ name, display_name, options }: IActionParamInfo): JSX.Element {
   return (
-    <Form.Item key={param.name} label={param.display_name} name={[param.name]}>
-      {renderFormItemStrategies?.[param?.val_type]?.(param)}
+    <Form.Item label={display_name} key={name}>
+      <Form.Item name={[name, "time_expr"]}>
+        <Input />
+      </Form.Item>
+      <Form.List name={[name, "time_format"]}>
+        {(fields, { add, remove }) => (
+          <Space align="start" className={classNames(styles.space, "scrollbar")}>
+            {fields.map((field) => (
+              <Space direction="vertical" key={field.key} align="center">
+                <Form.Item {...field}>
+                  <Select className={styles.select}>
+                    {options?.map((o) => (
+                      <Option key={o}>{o}</Option>
+                    ))}
+                  </Select>
+                  <div className={styles.center}>
+                    <MinusCircleOutlined onClick={() => remove(field.name)} />
+                  </div>
+                </Form.Item>
+              </Space>
+            ))}
+            <Button className={styles.buttonRight} icon={<PlusOutlined />} onClick={() => add()} />
+          </Space>
+        )}
+      </Form.List>
     </Form.Item>
   );
 }
 
-const renderFormItemStrategies: Record<string, (props: IActionParamInfo) => JSX.Element> = {
-  select: ({ options }) => (
-    <Select>
-      {options?.map((val) => (
-        <Option key={val} value={val}>
-          {val}
-        </Option>
-      ))}
-    </Select>
-  ),
-  str: () => <TextArea />,
-  source: SourceItem,
-};
-
-function SourceItem(): JSX.Element {
+function SourceItem({ name, display_name }: IActionParamInfo): JSX.Element {
   const { data, isLoading } = usePipelineSource();
   return (
-    <Select
-      loading={isLoading}
-      options={data}
-      showSearch
-      optionFilterProp="children"
-      filterOption={(input, option) =>
-        (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
-      }
-    />
+    <Form.Item key={name} label={display_name} name={[name]}>
+      <Select
+        loading={isLoading}
+        options={data}
+        showSearch
+        optionFilterProp="children"
+        filterOption={(input, option) =>
+          (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+        }
+      />
+    </Form.Item>
   );
 }
