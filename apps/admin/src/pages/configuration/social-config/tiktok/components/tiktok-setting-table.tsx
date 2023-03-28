813fc16e3e10bd4d79830d44078f4bddb6a1a5ee
Nguyễn  Khả <nguyendinhkha.ptit@gmail.com>
2023-03-28 03:13:13 +0000

    Fix social config

--- apps/admin/src/pages/configuration/social-config/tiktok/components/tiktok-setting-table.tsx
+++ apps/admin/src/pages/configuration/social-config/tiktok/components/tiktok-setting-table.tsx
@@ -55,7 +55,7 @@ export const TTSettingTable: React.FC<Props> = ({ adminData, data, loading }) =>
       title: "Các tài khoản theo dõi",
       dataIndex: "followed_by",
       render: (list_account: any) => {
-        return list_account.map((item: any) => (
+        return list_account?.map((item: any) => (
           <Space size={[0, 8]} wrap>
             <Tag>{item.username}</Tag>
           </Space>
